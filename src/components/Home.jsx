56f2b1910dc62b6c49f4369722351857e52cf9c8
Ayush Suryan <suryanayush@gmail.com>
2024-10-26 20:05:17 +0530

    done something

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,46 @@
+// src/pages/Home.jsx
 import React from "react";
 import "./Home.css";
+import heroImage from "../assets/hero.svg";
+import rupee from "../assets/rupee.svg";
+import arrow from "../assets/arrow.svg";
 const Home = () => {
-  return <div className="card">Hello</div>;
+  return (
+    <div
+      className="container"
+      style={{
+        fontFamily: "Arial, sans-serif",
+        backgroundColor: "#252433",
+        color: "#fff",
+        margin: 0,
+        position: "relative",
+      }}
+    >
+      <img
+        src={heroImage}
+        alt="Shadow"
+        style={{
+          position: "absolute",
+          width: "100%",
+          height: "90vh",
+          top: "-50px",
+          left: "50%",
+          transform: "translateX(-50%)",
+        }}
+      />
+      <div style={{ zIndex: 1, width: "90%%" }} className="bottomContainer">
+        <div
+          style={{ display: "flex", alignItems: "center" }}
+          className="contract"
+        >
+          <img src={rupee} alt="cash" />
+          <h2>Deposit / Withdraw</h2>
+
+          <img style={{ width: "30px" }} src={arrow} alt="cash" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -1,31 +1,96 @@
 import React from "react";
 import "./Order.css";
-import rigImage from "../assets/test.svg";
+import bronze from "../assets/bronze.svg";
+import silver from "../assets/silver.svg";
+import gold from "../assets/gold.svg";
+import platinum from "../assets/platinum.svg";
+import diamond from "../assets/diamond.svg";
+import legendary from "../assets/legendary.svg";
 import shadowImage from "../assets/shadow.svg";
 
 const Order = () => {
+  // Array of objects representing each card's data
+  const rigs = [
+    {
+      id: 1,
+      name: "Cubexo Bronze",
+      price: "60,000",
+      dailyReturn: "1,200",
+      duration: "90 Days",
+      img: bronze,
+    },
+    {
+      id: 2,
+      name: "Cubexo Silver",
+      price: "80,000",
+      dailyReturn: "1,500",
+      duration: "120 Days",
+      img: silver,
+    },
+    {
+      id: 3,
+      name: "Cubexo Gold",
+      price: "100,000",
+      dailyReturn: "2,000",
+      duration: "150 Days",
+      img: gold,
+    },
+    {
+      id: 4,
+      name: "Cubexo Platinum",
+      price: "150,000",
+      dailyReturn: "3,000",
+      duration: "150 Days",
+      img: platinum,
+    },
+    {
+      id: 5,
+      name: "Cubexo Diamon",
+      price: "150,000",
+      dailyReturn: "3,000",
+      duration: "150 Days",
+      img: diamond,
+    },
+    {
+      id: 6,
+      name: "Cubexo Platinum",
+      price: "150,000",
+      dailyReturn: "3,000",
+      duration: "150 Days",
+      img: legendary,
+    },
+
+    // Add more rig objects as needed
+  ];
+
   return (
     <div className="container">
       <div className="blurred-image-container">
         <img src={shadowImage} alt="Shadow" className="blurred-image" />
       </div>
       <div className="cards-container">
-        <div className="card">
-          <img src={rigImage} alt="rigImage" />
-          <div className="right">
-            <div className="right-item">
-              <span>Heading:</span>
-            </div>
-            <div className="right-item">
-              <span className="gray-text">Heading:</span>
-              <span className="red-text">Lorem, ipsum</span>
-            </div>
-            <div className="right-item">
-              <span className="gray-text">Heading:</span>
-              <span>Lorem, ipsum</span>
+        {rigs.map((rig) => (
+          <div className="card" key={rig.id}>
+            <img src={rig.img} alt={rig.name} />
+            <div className="right">
+              <div className="right-item">
+                <span>{rig.name}</span>
+              </div>
+              <div className="right-item">
+                <span className="gray-text">Price:</span>
+                <span className="red-text">{rig.price}</span>
+              </div>
+              <div className="right-item">
+                <span className="gray-text">Daily Return:</span>
+                <span>{rig.dailyReturn}</span>
+              </div>
+              <div className="right-item">
+                <span className="gray-text">Duration:</span>
+                <span>{rig.duration}</span>
+              </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
