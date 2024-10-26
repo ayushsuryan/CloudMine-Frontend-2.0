b2c43ff1e743b80c6cce6e75cabcfe21e7419c9f
Ayush Suryan <suryanayush@gmail.com>
2024-10-26 21:22:56 +0530

    done minor

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,19 @@
-// src/pages/Home.jsx
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
 import heroImage from "../assets/hero.svg";
 import rupee from "../assets/rupee.svg";
 import arrow from "../assets/arrow.svg";
+import deposit from "../assets/deposit.svg";
+import withdraw from "../assets/withdraw.svg";
+
 const Home = () => {
+  const [balance, setBalance] = useState("70,000");
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleContainer = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
     <div
       className="container"
@@ -28,17 +37,75 @@ const Home = () => {
           transform: "translateX(-50%)",
         }}
       />
-      <div style={{ zIndex: 1, width: "90%%" }} className="bottomContainer">
-        <div
-          style={{ display: "flex", alignItems: "center" }}
-          className="contract"
-        >
-          <img src={rupee} alt="cash" />
-          <h2>Deposit / Withdraw</h2>
 
-          <img style={{ width: "30px" }} src={arrow} alt="cash" />
+      {!isExpanded ? (
+        <div
+          style={{
+            zIndex: 1,
+            width: "90%",
+            position: "absolute",
+            bottom: "150px",
+          }}
+          className="bottomContainer"
+          onClick={toggleContainer}
+        >
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              padding: "20px 15px",
+            }}
+            className="contract"
+          >
+            <img src={rupee} alt="cash" />
+            <h2>{balance} INR</h2>
+            <img style={{ width: "30px" }} src={arrow} alt="cash" />
+          </div>
         </div>
-      </div>
+      ) : (
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-evenly",
+            height: "300px",
+            position: "absolute",
+            bottom: "70px",
+            zIndex: 1,
+            width: "80%",
+          }}
+          className="bottomContainer"
+        >
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+
+              padding: "20px 15px",
+            }}
+            className="contract"
+          >
+            <img style={{ width: "40px" }} src={deposit} alt="cash" />
+            <h2>Deposit</h2>
+            <img style={{ width: "30px" }} src={arrow} alt="cash" />
+          </div>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              padding: "20px 15px",
+            }}
+            className="contract"
+          >
+            <img style={{ width: "40px" }} src={withdraw} alt="cash" />
+            <h2>Withdraw</h2>
+            <img style={{ width: "30px" }} src={arrow} alt="cash" />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
